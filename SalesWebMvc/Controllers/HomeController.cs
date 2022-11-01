60afd8acda9f430c02157a29093ef1b6af01b519
Raíssa Juliana <rjgalbino@uniara.edu.br>
2022-11-01 16:10:59 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
+            ViewData["Student"] = "Raíssa Juliana";
 
             return View();
         }
